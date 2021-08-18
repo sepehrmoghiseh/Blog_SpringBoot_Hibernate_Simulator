6f3ff02b59734ae2917d166c287fada88e93a6af
Sepehr Moghiseh <35396646+sepehrmoghiseh@users.noreply.github.com>
2021-08-18 12:19:36 +0430

    Delete src/main/java/repository directory

--- src/main/java/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import domain.PostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PostRepository extends JpaRepository<PostEntity,Integer> {
-}
